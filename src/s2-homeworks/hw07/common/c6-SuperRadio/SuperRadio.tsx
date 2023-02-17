96e2f5f4579d070bcc770edfb10b4a800cd50aa5
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2023-02-17 14:07:50 +0300

    home works №7/4

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -51,7 +51,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      value={value}
+                      value={o.id}
                       checked={o.id === value}
                       name={"radio"}
                       // name, checked, value делают студенты
