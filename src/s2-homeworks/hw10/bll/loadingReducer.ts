5a2f3f4cad6b4264bf33a210a943bb69aa239c27
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2023-03-13 19:39:19 +0300

    home works №10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,9 +1,17 @@
-const initState = {
+
+export type stateInitType = {
+    isLoading: boolean,
+}
+const initState: stateInitType = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): stateInitType => { // fix any
     switch (action.type) {
+        case 'CHANGE_LOADING':
+            state.isLoading = action.isLoading
+            return {...state, isLoading: !state.isLoading}
+
         // пишет студент  // need to fix
 
         default:
